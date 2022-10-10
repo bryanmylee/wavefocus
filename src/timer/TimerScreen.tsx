b65fbe7ccffef44a64605ec4fd8255fa74bd6db7
Bryan Lee <bryanmylee@gmail.com>
2022-10-10 12:15:45 +1100

    fix: show next button on complete

--- src/timer/TimerScreen.tsx
+++ src/timer/TimerScreen.tsx
@@ -66,7 +66,7 @@ export default function TimerScreen({onPressLoginButton}: TimerScreenProps) {
 				) : (
 					<IconPlaceholder />
 				)}
-				{!isActive && !isDone ? (
+				{!isActive ? (
 					<NextButton onPress={handleNextPress} />
 				) : (
 					<IconPlaceholder />
