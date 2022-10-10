b226bb1bb0b9142af28eaf9f6cf67813dcdd2394
Bryan Lee <bryanmylee@gmail.com>
2022-10-10 12:38:59 +1100

    feat: sign in with google

--- src/auth/LoginScreen.tsx
+++ src/auth/LoginScreen.tsx
@@ -1,28 +1,43 @@
-import React, {useState} from 'react';
+import React, {useCallback} from 'react';
+import auth from '@react-native-firebase/auth';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import {Button} from 'react-native';
 import 'react-native-gesture-handler';
 import styled from 'styled-components/native';
 import DismissButton from '../core/DismissButton';
 import FixedSafeAreaView from '../core/FixedSafeAreaView';
-import StyledTextInput from '../core/StyledTextInput';
 import {useBackHandler} from '../utils/useBackHandler';
+
+GoogleSignin.configure({
+	webClientId:
+		'843057937567-v8tqhtkak5qmvnjf5j873dkn2svugn19.apps.googleusercontent.com',
+});
+
+async function onGoogleButtonPress() {
+	const {idToken} = await GoogleSignin.signIn();
+	const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+	return auth().signInWithCredential(googleCredential);
+}
 
 interface LoginScreenProps {
 	onDismiss?: () => void;
 }
 
 export default function LoginScreen({onDismiss}: LoginScreenProps) {
-	const [email, setEmail] = useState('');
 	useBackHandler(() => {
 		onDismiss?.();
 		return true;
 	}, [onDismiss]);
+	const onLoginButtonPress = useCallback(async () => {
+		await onGoogleButtonPress();
+	}, []);
 	return (
 		<FixedSafeAreaView>
 			<TopBar>
 				<DismissButton onPress={onDismiss} />
 			</TopBar>
 			<LoginFormContainer>
-				<StyledTextInput value={email} onChangeText={setEmail} width="80%" />
+				<Button title="Sign in with Google" onPress={onLoginButtonPress} />
 			</LoginFormContainer>
 		</FixedSafeAreaView>
 	);
