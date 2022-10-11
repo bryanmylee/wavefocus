30b8d9f5132c7c39d77f43325cd28cafc5b8ecfc
Bryan Lee <bryanmylee@gmail.com>
2022-10-11 17:49:27 +1100

    style: remove dy animation for focus

--- src/timer/ActiveFocusAnimation.tsx
+++ src/timer/ActiveFocusAnimation.tsx
@@ -7,12 +7,12 @@ import Animated, {
 	useAnimatedProps,
 	useDerivedValue,
 	useSharedValue,
-	withDelay,
 	withTiming,
 } from 'react-native-reanimated';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {Circle} from 'react-native-svg';
 import {useTheme} from 'styled-components/native';
+import {useOscillatingValue} from '../utils/useOscillatingValue';
 
 const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
@@ -34,9 +34,9 @@ export default function ActiveFocusAnimation({
 	const radius = Math.min(width, height) * 0.4;
 	return (
 		<>
-			<FocusCircle radius={radius} showProgress={showProgress} />
-			<FocusCircle radius={radius} showProgress={showProgress} delayMs={50} />
-			<FocusCircle radius={radius} showProgress={showProgress} delayMs={100} />
+			<FocusCircle radius={radius} showProgress={showProgress} cycleMs={1000} />
+			<FocusCircle radius={radius} showProgress={showProgress} cycleMs={1333} />
+			<FocusCircle radius={radius} showProgress={showProgress} cycleMs={1750} />
 		</>
 	);
 }
@@ -44,37 +44,47 @@ export default function ActiveFocusAnimation({
 interface FocusCircleProps {
 	radius: number;
 	showProgress: SharedValue<number>;
-	delayMs?: number;
+	cycleMs: number;
 }
 
-function FocusCircle({radius, showProgress, delayMs = 0}: FocusCircleProps) {
+type Point = [number, number];
+function getPointFromTheta(theta: number, d: number): Point {
+	'worklet';
+	return [d * Math.cos(theta), d * Math.sin(theta)];
+}
+
+function FocusCircle({radius, showProgress, cycleMs}: FocusCircleProps) {
 	const theme = useTheme();
 	const insets = useSafeAreaInsets();
 	const {width, height} = useWindowDimensions();
 	const cx = insets.left + (width - insets.left - insets.right) / 2;
 	const cy = insets.top + (height - insets.top - insets.bottom) / 2;
+
+	const theta = useOscillatingValue({
+		from: 0,
+		to: Math.PI * 4,
+		cycleMs: cycleMs * 2,
+	});
+	const wiggleRadius = radius * 0.05;
+	const wiggle = useDerivedValue(() =>
+		getPointFromTheta(theta.value, wiggleRadius),
+	);
+
 	const scale = useDerivedValue(() =>
 		withTiming(interpolate(showProgress.value, [0, 1], [0, 1]), {
 			duration: 1000,
-			easing: Easing.inOut(Easing.elastic(1.2)),
+			easing: Easing.elastic(1),
 		}),
 	);
-	const dy = useDerivedValue(() =>
-		withDelay(
-			delayMs,
-			withTiming(interpolate(showProgress.value, [0, 1], [-height / 1.5, 0]), {
-				duration: 1000,
-				easing: Easing.inOut(Easing.cubic),
-			}),
-		),
-	);
+
 	const circleAnimProps = useAnimatedProps(() => ({
 		r: radius * scale.value,
-		cy: cy + dy.value,
+		cx: cx + wiggle.value[0],
+		cy: cy + wiggle.value[1],
 	}));
+
 	return (
 		<AnimatedCircle
-			cx={cx}
 			fill={theme.timer.fluidFill}
 			opacity={theme.timer.fluidOpacity}
 			animatedProps={circleAnimProps}
