3de1625ff0705b21556e0f184875cd3effbabbf2
Bryan Lee <bryanmylee@gmail.com>
2022-10-11 16:24:00 +1100

    feat: centered fixed safe area

--- src/components/FixedSafeAreaView.tsx
+++ src/components/FixedSafeAreaView.tsx
@@ -2,8 +2,23 @@ import React, {PropsWithChildren} from 'react';
 import {EdgeInsets, useSafeAreaInsets} from 'react-native-safe-area-context';
 import styled from 'styled-components/native';
 
-export default function FixedSafeAreaView({children}: PropsWithChildren) {
+interface FixedSafeAreaViewProps extends PropsWithChildren {
+	centered?: boolean;
+}
+
+export default function FixedSafeAreaView({
+	centered = false,
+	children,
+}: FixedSafeAreaViewProps) {
 	const insets = useSafeAreaInsets();
+	if (centered) {
+		const vert = Math.max(insets.top, insets.bottom);
+		insets.top = vert;
+		insets.bottom = vert;
+		const hori = Math.max(insets.left, insets.right);
+		insets.left = hori;
+		insets.right = hori;
+	}
 	return <Container insets={insets}>{children}</Container>;
 }
 
