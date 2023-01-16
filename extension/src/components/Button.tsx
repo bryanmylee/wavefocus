6ffdc357da13ee617da68152f508fd9b0c3d86ff
Bryan Lee <bryanmylee@gmail.com>
2023-01-16 18:13:16 +0800

    style: reduce button size

--- extension/src/components/Button.tsx
+++ extension/src/components/Button.tsx
@@ -1,4 +1,5 @@
 import React, {ButtonHTMLAttributes} from 'react';
+import {twMerge} from 'tailwind-merge';
 
 const Button = React.forwardRef<
 	HTMLButtonElement,
@@ -9,10 +10,10 @@ const Button = React.forwardRef<
 		<button
 			ref={forwardedRef}
 			{...restProps}
-			className={
-				'transition-colors rounded-2xl bg-button-fill px-3 py-[7.5px] font-medium text-button-text text-base tracking-tight hover:opacity-75 active:opacity-50' +
-				className
-			}>
+			className={twMerge(
+				'transition-colors rounded-xl bg-button-fill px-3 py-[5px] font-medium text-button-text text-sm tracking-tight hover:opacity-75 active:opacity-50',
+				className,
+			)}>
 			{children}
 		</button>
 	);
